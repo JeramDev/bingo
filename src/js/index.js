78fcbf70e6ce9a183f69ea18293341cb7d45884c
JeramDev <jesusramosdev@gmail.com>
2019-09-30 16:10:18 +0100

    Haciendo bingo para más jugadores

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,16 @@
-const botonSacar = document.querySelector('.botonSacar');
-const numeroMostrado = document.querySelector('.valor');
+const TOTALBOLAS = 90;
+const ITEMSCARTON = 15;
+const NUMEROJUGADORES = 5;
+
+const dom = {
+    juego: document.querySelector('.bingo'),
+    botonSacar: document.querySelector('.botonSacar'),
+    numeroMostrado: document.querySelector('.valor')
+}
 
 // Crear el bombo
 const crearBombo = () => {
-    let numerosBombo = _.range(1, 91);
+    let numerosBombo = _.range(1, TOTALBOLAS + 1);
     numerosBombo = _.shuffle(numerosBombo);
     return numerosBombo;    
 }
@@ -11,13 +18,25 @@ const crearBombo = () => {
 const bombo = crearBombo();
 
 // Crear y mostrar el carton
-const crearCarton = (player) => {
-    const carton = document.querySelector(`.carton.${player}`);
-    let numerosCarton = _.range(1, 91);
-    numerosCarton = _.shuffle(numerosCarton);
-    numerosCarton = numerosCarton.slice(0,15);
-    mostrarCarton(carton, numerosCarton);
-    return carton;
+const crearCartones = (items = ITEMSCARTON, numJugadores = NUMEROJUGADORES) => {
+    for (let i = 0; i < numJugadores; i++) {
+        dom.juego.innerHTML +=
+            `<div class="panel">
+                <p class="nombreJugador">Jugador ${i + 1}</p>
+                <div class="carton jugador">
+                
+                </div>
+            </div>`;
+    }
+    const cartones = document.querySelectorAll(`.carton`); 
+
+    cartones.forEach((carton) => {
+        let numerosCarton = _.range(1, TOTALBOLAS + 1);
+        numerosCarton = _.shuffle(numerosCarton);
+        numerosCarton = numerosCarton.slice(0, items);
+        mostrarCarton(carton, numerosCarton);
+    });
+    
 }
 
 const mostrarCarton = (carton, numeros) => {
@@ -38,9 +57,8 @@ const tacharNumero = (numero) => {
 
 // Muestra el resultado al terminar el juego
 const mostrarGanador = (nombreGanador) => {
-    const juego = document.querySelector('.bingo');
-    juego.innerHTML = `<p class="textoGanador">GANADOR: ${nombreGanador.toUpperCase()}</div>`;
-    juego.classList.toggle('win');
+    dom.juego.innerHTML = `<p class="textoGanador">GANADOR: ${nombreGanador.toUpperCase()}</div>`;
+    dom.juego.classList.toggle('win');
 }
 
 // Comprueba si algun jugador ha ganado
@@ -48,26 +66,30 @@ const checkWin = () => {
     const cartones = document.querySelectorAll('.carton');
     cartones.forEach((car) => {        
         const numerosTachados = car.querySelectorAll('.tachado').length;        
-        if (numerosTachados == 15) {
+        if (numerosTachados == ITEMSCARTON) {
             mostrarGanador(car.classList.item(1));
         }            
     });
 }
 
 // Sacar una bola del bombo
-const sacarBola = () => {      
+const sacarBola = () => {  
+    console.log(bombo);
+    checkWin(); 
     const numeroSacado = bombo.pop();    
-    numeroMostrado.textContent = numeroSacado;
+    dom.numeroMostrado.textContent = numeroSacado;
     tacharNumero(numeroSacado);    
 }
 
+const hola = () => {
+    console.log('hola');
+}
 // Boton
-botonSacar.addEventListener('click', sacarBola);
+dom.botonSacar.addEventListener('click', hola);
 
 // Iniciar juego
 const iniciarJuego = () => {
-    crearCarton("jugador");   
-    crearCarton("cpu");  
+    crearCartones(ITEMSCARTON);  
 }
 
 iniciarJuego();
